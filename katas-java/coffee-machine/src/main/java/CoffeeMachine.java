3b68532e6798dc9b0480c2381e9c65fac5298c3a
Toño de la Torre <antonio@codesai.com>
2022-09-09 10:39:58 +0200

    Red test. Just tea.

---
+++ katas-java/coffee-machine/src/main/java/CoffeeMachine.java
@@ -0,0 +1,25 @@
+public class CoffeeMachine {
+
+  public CoffeeMachine(DrinkMaker drinkMaker) {
+
+  }
+
+  public CoffeeMachine(DrinkMaker drinkMaker, DrinkMakerProtocol drinkMakerProtocol) {
+
+  }
+
+  public void tea() {
+  }
+
+  public void addSugar() {
+  }
+
+  public void make() {
+  }
+
+  public void chocolate() {
+  }
+
+  public void coffee() {
+  }
+}
--- katas-java/coffee-machine/src/test/java/CoffeeMachineAcceptanceTest.java
+++ katas-java/coffee-machine/src/test/java/CoffeeMachineAcceptanceTest.java
@@ -9,11 +9,11 @@ public class CoffeeMachineAcceptanceTest {
   @Test
   public void tea_with_sugar() {
     DrinkMaker drinkMaker = mock(DrinkMaker.class);
-    CoffeeMaker coffeeMaker = new CoffeeMaker(drinkMaker);
+    CoffeeMachine coffeeMachine = new CoffeeMachine(drinkMaker);
 
-    coffeeMaker.tea();
-    coffeeMaker.addSugar();
-    coffeeMaker.make();
+    coffeeMachine.tea();
+    coffeeMachine.addSugar();
+    coffeeMachine.make();
 
     verify(drinkMaker).execute("T:1:0");
   }
@@ -21,10 +21,10 @@ public class CoffeeMachineAcceptanceTest {
   @Test
   public void hot_chocolate_without_sugar() {
     DrinkMaker drinkMaker = mock(DrinkMaker.class);
-    CoffeeMaker coffeeMaker = new CoffeeMaker(drinkMaker);
+    CoffeeMachine coffeeMachine = new CoffeeMachine(drinkMaker);
 
-    coffeeMaker.chocolate();
-    coffeeMaker.make();
+    coffeeMachine.chocolate();
+    coffeeMachine.make();
 
     verify(drinkMaker).execute("H::");
   }
@@ -32,12 +32,12 @@ public class CoffeeMachineAcceptanceTest {
   @Test
   public void coffee_with_two_sugars() {
     DrinkMaker drinkMaker = mock(DrinkMaker.class);
-    CoffeeMaker coffeeMaker = new CoffeeMaker(drinkMaker);
+    CoffeeMachine coffeeMachine = new CoffeeMachine(drinkMaker);
 
-    coffeeMaker.coffee();
-    coffeeMaker.addSugar();
-    coffeeMaker.addSugar();
-    coffeeMaker.make();
+    coffeeMachine.coffee();
+    coffeeMachine.addSugar();
+    coffeeMachine.addSugar();
+    coffeeMachine.make();
 
     verify(drinkMaker).execute("C:2:0");
   }
