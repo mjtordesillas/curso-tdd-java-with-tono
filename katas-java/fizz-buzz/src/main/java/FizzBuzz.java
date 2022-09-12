dc84ea9732bed3df2b1b446e03d0178dfc8b7f07
mjtordesillas <mjtordesillas@codesai.com>
2022-09-12 09:03:30 +0200

    refactor: extracted method for readability

--- katas-java/fizz-buzz/src/main/java/FizzBuzz.java
+++ katas-java/fizz-buzz/src/main/java/FizzBuzz.java
@@ -5,10 +5,14 @@ public class FizzBuzz {
   public List<String> fizzBuzzle() {
     List<String> fizzBuzzed = new ArrayList<String>();
     for(Integer number = 1; number <= 4; number++) {
-      if(number == 3) fizzBuzzed.add("Fizz");
+      if(isFizz(number)) fizzBuzzed.add("Fizz");
       else fizzBuzzed.add(number.toString());
     }
 
     return fizzBuzzed;
+  }
+
+  private boolean isFizz(Integer number) {
+    return number == 3;
   }
 }
